4671cf208e7f54b2be92942d2bb831f6a12a24bf
Timofey Mukha <timofey.mukha@it.uu.se>
2016-03-28 18:57:24 +0200

    Commented out legacy lund_rescaling function an placed it in the end of the file.

--- eddylicious/generators/lund_rescaling.py
+++ eddylicious/generators/lund_rescaling.py
@@ -228,148 +228,7 @@ def lund_rescale_fluctuations(etaPrec, yPlusPrec, pointsZ,
     return [uPrimeXInfl, uPrimeYInfl, uPrimeZInfl]
 
 
-def lund_generate_legacy(reader, readPath,
-                         writer, writePath,
-                         dt, t0, tEnd, timePrecision,
-                         uMeanPrec, uMeanInfl,
-                         etaPrec, yPlusPrec, pointsZ,
-                         etaInfl, yPlusInfl, pointsZInfl,
-                         nInfl, nInner, gamma,
-                         yInd, zInd,
-                         surfaceName="None", times="None"):
-    """An older serial version of lund_generate kept here just in case.
 
-
-    Parameters
-    ----------
-    reader : str
-        The name of the reader which will be used to read the
-        velocity field from the precursor simulation.
-    readPath : str
-        The path for the reader.
-    writer: str
-        The writer that will be used to save the values of the
-        velocity field.
-    writePath : str
-        The path for the writer
-    dt : float
-        The time-step to be used in the simulation. This will be
-        used to associate a time-value with the produced velocity
-        fields.
-    t0 : float
-        The starting time to be used in the simulation. This will
-        be used to associate a time-value with the produced velocity
-    timePrecision : int
-        Number of points after the decimal to keep for the time value.
-    tEnd : float
-        The ending time for the simulation.
-    uMeanPrec : 1d ndarray
-        The values of the mean velocity from the precursor.
-    uMeanInfl : 1d ndarray
-        The values of the mean velocity for the inflow boundary
-        layer.
-    etaPrec : ndarray
-        The values of eta for the corresponding values
-        of the mean velocity from the precursor.
-    yPlusPrec : ndarray
-        The values of y+ for the corresponding values
-        of the mean velocity from the precursor.
-    pointsZ : ndarray
-        A 2d array containing the values of z for the points
-        of the precuror mesh.
-    etaInfl : 1d ndarray
-        The values of eta for the mesh points at the inflow
-        boundary.
-    yPlusInfl : 1d ndarray
-        The values of y+ for the meshpoints at the inflow
-        boundary.
-    pointsZInfl : int
-        A 2d array containing the values of z for the points
-        of the inflow boundary.
-    nInfl : int
-        The amount of points in the wall-normal direction
-        that contain the boundary layer at the inflow
-        boundary. That is, for points beyound nInfl, 0 will
-        be prescribed.
-    nInner : int
-        The amount of points where inner rescaling should be
-        considered. For points beyound nInner, the outer
-        rescaling only we be computed. The relaxes the demand
-        on Re_tau for the precursor.
-    gamma : float
-        The ration of the friction velocities in the inflow
-        boundary layer and the precursor.
-    yInd : ndarray
-        The sort indices for sorting the read velocity field.
-        This is needed when some sorting is performed when the
-        mesh points are read and turned into ordered 2d arrays.
-        Them the exact same sorting should be applyed to the
-        velocity fields.
-    zInd : ndarray
-        Same as yInd, but for the sorting of the z values.
-    surfaceName : str, optional
-        For the foamFile reader, the name of the surface used for
-        sampling the velocity values.
-    times : list of floats, optional
-        For the foamFile reader, the times for which the velocity
-        field was sampled in the precursor simulation.
-    """
-
-    t = t0
-
-    readTimeI = 0
-    writeTimeI = 0
-    size = int((tEnd-t0)/dt+1)
-
-    while (t <= tEnd):
-        print "    Generating for time", t
-
-        # Read U data
-        if (reader == "foamFile"):
-            [U_X, U_Y, U_Z] = read_u_from_foamfile(
-                os.path.join(readPath, times[readTimeI], surfaceName,
-                             "vectorField", "U"),
-                pointsZ.shape[0], pointsZ.shape[1],
-                yInd, zInd)
-            readTimeI += 1
-
-            if (readTimeI == len(times)):
-                readTimeI = 0
-        else:
-            print "ERROR. Unknown reader ", reader
-            exit()
-
-        # Claculate UPrime
-        uPrimeX = U_X - uMeanPrec[:, np.newaxis]
-        uPrimeY = U_Y
-        uPrimeZ = U_Z
-
-        [uPrimeXInfl, uPrimeYInfl, uPrimeZInfl] = \
-            lund_rescale_fluctuations(
-                etaPrec, yPlusPrec, pointsZ,
-                uPrimeX, uPrimeY, uPrimeZ, gamma,
-                etaInfl, yPlusInfl, pointsZInfl,
-                nInfl, nInner)
-
-        # Combine and flatten
-        UInfl_X = np.reshape(uPrimeXInfl+uMeanInfl, (uPrimeXInfl.size, -1),
-                             order='F')
-        UInfl_Y = np.reshape(uPrimeYInfl, (uPrimeXInfl.size, -1), order='F')
-        UInfl_Z = np.reshape(uPrimeZInfl, (uPrimeXInfl.size, -1), order='F')
-
-        UInfl = np.concatenate((UInfl_X, UInfl_Y, UInfl_Z), axis=1)
-
-        if (writer == "tvmfv"):
-            write_u_to_tvmfv(writePath, t, UInfl)
-        elif (writer == "hdf5"):
-            write_u_to_hdf5(writePath, t, UInfl, writeTimeI, size)
-        else:
-            print "ERROR in lund_generate(). Unknown writer ", writer
-            exit()
-
-        writeTimeI += 1
-        t += dt
-        t = float(("{0:."+str(timePrecision)+"f}").format(t))
 
 def lund_generate(reader, readPath,
                   writer, writePath,
@@ -535,3 +394,146 @@ def lund_generate(reader, readPath,
         else:
             print "ERROR in lund_generate(). Unknown writer ", writer
             exit()
+
+#def lund_generate_legacy(reader, readPath,
+#                         writer, writePath,
+#                         dt, t0, tEnd, timePrecision,
+#                         uMeanPrec, uMeanInfl,
+#                         etaPrec, yPlusPrec, pointsZ,
+#                         etaInfl, yPlusInfl, pointsZInfl,
+#                         nInfl, nInner, gamma,
+#                         yInd, zInd,
+#                         surfaceName="None", times="None"):
+#    """An older serial version of lund_generate kept here just in case.
+#
+#
+#    Parameters
+#    ----------
+#    reader : str
+#        The name of the reader which will be used to read the
+#        velocity field from the precursor simulation.
+#    readPath : str
+#        The path for the reader.
+#    writer: str
+#        The writer that will be used to save the values of the
+#        velocity field.
+#    writePath : str
+#        The path for the writer
+#    dt : float
+#        The time-step to be used in the simulation. This will be
+#        used to associate a time-value with the produced velocity
+#        fields.
+#    t0 : float
+#        The starting time to be used in the simulation. This will
+#        be used to associate a time-value with the produced velocity
+#    timePrecision : int
+#        Number of points after the decimal to keep for the time value.
+#    tEnd : float
+#        The ending time for the simulation.
+#    uMeanPrec : 1d ndarray
+#        The values of the mean velocity from the precursor.
+#    uMeanInfl : 1d ndarray
+#        The values of the mean velocity for the inflow boundary
+#        layer.
+#    etaPrec : ndarray
+#        The values of eta for the corresponding values
+#        of the mean velocity from the precursor.
+#    yPlusPrec : ndarray
+#        The values of y+ for the corresponding values
+#        of the mean velocity from the precursor.
+#    pointsZ : ndarray
+#        A 2d array containing the values of z for the points
+#        of the precuror mesh.
+#    etaInfl : 1d ndarray
+#        The values of eta for the mesh points at the inflow
+#        boundary.
+#    yPlusInfl : 1d ndarray
+#        The values of y+ for the meshpoints at the inflow
+#        boundary.
+#    pointsZInfl : int
+#        A 2d array containing the values of z for the points
+#        of the inflow boundary.
+#    nInfl : int
+#        The amount of points in the wall-normal direction
+#        that contain the boundary layer at the inflow
+#        boundary. That is, for points beyound nInfl, 0 will
+#        be prescribed.
+#    nInner : int
+#        The amount of points where inner rescaling should be
+#        considered. For points beyound nInner, the outer
+#        rescaling only we be computed. The relaxes the demand
+#        on Re_tau for the precursor.
+#    gamma : float
+#        The ration of the friction velocities in the inflow
+#        boundary layer and the precursor.
+#    yInd : ndarray
+#        The sort indices for sorting the read velocity field.
+#        This is needed when some sorting is performed when the
+#        mesh points are read and turned into ordered 2d arrays.
+#        Them the exact same sorting should be applyed to the
+#        velocity fields.
+#    zInd : ndarray
+#        Same as yInd, but for the sorting of the z values.
+#    surfaceName : str, optional
+#        For the foamFile reader, the name of the surface used for
+#        sampling the velocity values.
+#    times : list of floats, optional
+#        For the foamFile reader, the times for which the velocity
+#        field was sampled in the precursor simulation.
+#    """
+#
+#    t = t0
+#
+#    readTimeI = 0
+#    writeTimeI = 0
+#    size = int((tEnd-t0)/dt+1)
+#
+#    while (t <= tEnd):
+#        print "    Generating for time", t
+#
+#        # Read U data
+#        if (reader == "foamFile"):
+#            [U_X, U_Y, U_Z] = read_u_from_foamfile(
+#                os.path.join(readPath, times[readTimeI], surfaceName,
+#                             "vectorField", "U"),
+#                pointsZ.shape[0], pointsZ.shape[1],
+#                yInd, zInd)
+#            readTimeI += 1
+#
+#            if (readTimeI == len(times)):
+#                readTimeI = 0
+#        else:
+#            print "ERROR. Unknown reader ", reader
+#            exit()
+#
+#        # Claculate UPrime
+#        uPrimeX = U_X - uMeanPrec[:, np.newaxis]
+#        uPrimeY = U_Y
+#        uPrimeZ = U_Z
+#
+#        [uPrimeXInfl, uPrimeYInfl, uPrimeZInfl] = \
+#            lund_rescale_fluctuations(
+#                etaPrec, yPlusPrec, pointsZ,
+#                uPrimeX, uPrimeY, uPrimeZ, gamma,
+#                etaInfl, yPlusInfl, pointsZInfl,
+#                nInfl, nInner)
+#
+#        # Combine and flatten
+#        UInfl_X = np.reshape(uPrimeXInfl+uMeanInfl, (uPrimeXInfl.size, -1),
+#                             order='F')
+#        UInfl_Y = np.reshape(uPrimeYInfl, (uPrimeXInfl.size, -1), order='F')
+#        UInfl_Z = np.reshape(uPrimeZInfl, (uPrimeXInfl.size, -1), order='F')
+#
+#        UInfl = np.concatenate((UInfl_X, UInfl_Y, UInfl_Z), axis=1)
+#
+#        if (writer == "tvmfv"):
+#            write_u_to_tvmfv(writePath, t, UInfl)
+#        elif (writer == "hdf5"):
+#            write_u_to_hdf5(writePath, t, UInfl, writeTimeI, size)
+#        else:
+#            print "ERROR in lund_generate(). Unknown writer ", writer
+#            exit()
+#
+#        writeTimeI += 1
+#        t += dt
+#        t = float(("{0:."+str(timePrecision)+"f}").format(t))
